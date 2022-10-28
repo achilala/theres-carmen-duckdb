6735f7eb876c5c16e26cd04c8e94ead603c6638a
Aka Chilala <Aka.Chilala@gmail.com>
2022-10-28 20:42:58 +1300

    sampling more of the schema tables

--- query_interpol_db.py
+++ query_interpol_db.py
@@ -1,11 +1,24 @@
 import duckdb
 
-db = "interpol_db.duckdb"
-sql = """
-select *
-  from mart.fct_sighting
- limit 500
-"""
-con = duckdb.connect(db, read_only=False)
-res = con.execute(sql).fetchdf()
-print(res)
+sql_stmts = [
+  "DESCRIBE",
+  """
+  select *
+    from mart.fct_sighting
+   limit 10
+  """,
+  """
+  select *
+    from mart.dim_witness
+   limit 10
+  """,
+  """
+  select *
+    from mart.dim_agent
+   limit 10
+  """
+]
+
+con = duckdb.connect("interpol_db.duckdb", read_only=False)
+
+[ print(con.execute(sql).fetchdf()) for sql in sql_stmts ]
